0ca43c17d8144b523930c06f7a1b100f517f3546
Eduardo Niehues <edanniehues@gmail.com>
2014-06-22 23:53:00 -0300

    Updated sim_info.py for AC 0.20

--- sm_nfo/sim_info.py
+++ sm_nfo/sim_info.py
@@ -1,7 +1,33 @@
+"""
+Assetto Corsa shared memory for Python applications
+
+_ctypes.pyd must be somewhere in sys.path, because AC doesn't include all Python binaries.
+
+Usage. Let's say you have following folder structure::
+
+    some_app
+        DLLs
+            _ctypes.pyd
+        some_app.py
+
+some_app.py::
+
+    import os
+    import sys
+    sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'DLLs'))
+
+    from sim_info import info
+
+    print(info.graphics.tyreCompound, info.physics.rpms, info.static.playerNick)
+
+
+Do whatever you want with this code!
+WBR, Rombik :)
+"""
 import mmap
 import functools
 import ctypes
-from ctypes import c_int32, c_float, c_char, c_wchar, c_bool, c_int
+from ctypes import c_int32, c_float, c_char
 
 
 AC_STATUS = c_int32
@@ -23,12 +49,12 @@ AC_DRAG = 6
 class SPageFilePhysics(ctypes.Structure):
     _pack_ = 4
     _fields_ = [
-        ('packetId' , c_int32),
+        ('packetId', c_int32),
         ('gas', c_float),
         ('brake', c_float),
         ('fuel', c_float),
-        ('gear' , c_int32),
-        ('rpms' , c_int32),
+        ('gear', c_int32),
+        ('rpms', c_int32),
         ('steerAngle', c_float),
         ('speedKmh', c_float),
         ('velocity', c_float * 3),
@@ -49,8 +75,8 @@ class SPageFilePhysics(ctypes.Structure):
         ('roll', c_float),
         ('cgHeight', c_float),
         ('carDamage', c_float * 5),
-        ('numberOfTyresOut' , c_int32),
-        ('pitLimiterOn' , c_int32),
+        ('numberOfTyresOut', c_int32),
+        ('pitLimiterOn', c_int32),
         ('abs', c_float),
     ]
 
@@ -58,26 +84,26 @@ class SPageFilePhysics(ctypes.Structure):
 class SPageFileGraphic(ctypes.Structure):
     _pack_ = 4
     _fields_ = [
-        ('packetId' , c_int32),
+        ('packetId', c_int32),
         ('status', AC_STATUS),
         ('session', AC_SESSION_TYPE),
          # NOTE: if you want str instead bytes, access it without '_'
-        ('_currentTime', c_char * 10),
-        ('_lastTime', c_char * 10), 
-        ('_bestTime', c_char * 10),
-        ('_split', c_char * 10),
-        ('completedLaps' , c_int32),
-        ('position' , c_int32),
-        ('iCurrentTime' , c_int32),
-        ('iLastTime' , c_int32),
-        ('iBestTime' , c_int32),
+        ('_currentTime', c_char * 15),
+        ('_lastTime', c_char * 15),
+        ('_bestTime', c_char * 15),
+        ('_split', c_char * 15),
+        ('completedLaps', c_int32),
+        ('position', c_int32),
+        ('iCurrentTime', c_int32),
+        ('iLastTime', c_int32),
+        ('iBestTime', c_int32),
         ('sessionTimeLeft', c_float),
         ('distanceTraveled', c_float),
-        ('isInPit' , c_int32),
-        ('currentSectorIndex' , c_int32),
-        ('lastSectorTime' , c_int32),
-        ('numberOfLaps' , c_int32),
-        ('_tyreCompound', c_char * 32),
+        ('isInPit', c_int32),
+        ('currentSectorIndex', c_int32),
+        ('lastSectorTime', c_int32),
+        ('numberOfLaps', c_int32),
+        ('_tyreCompound', c_char * 33),
 
         ('replayTimeMultiplier', c_float),
         ('normalizedCarPosition', c_float),
@@ -88,22 +114,22 @@ class SPageFileGraphic(ctypes.Structure):
 class SPageFileStatic(ctypes.Structure):
     _pack_ = 4
     _fields_ = [
-        ('_smVersion', c_char * 10),
-        ('_acVersion', c_char * 10),
+        ('_smVersion', c_char * 15),
+        ('_acVersion', c_char * 15),
         # session static info
-        ('numberOfSessions' , c_int32),
-        ('numCars' , c_int32),
-        ('_carModel', c_char * 32),
-        ('_track', c_char * 32),
-        ('_playerName', c_char * 32),
-        ('_playerSurname', c_char * 32),
-        ('_playerNick', c_char * 32),
-        ('sectorCount' , c_int32),
+        ('numberOfSessions', c_int32),
+        ('numCars', c_int32),
+        ('_carModel', c_char * 33),
+        ('_track', c_char * 33),
+        ('_playerName', c_char * 33),
+        ('_playerSurname', c_char * 33),
+        ('_playerNick', c_char * 33),
+        ('sectorCount', c_int32),
 
         # car static info
         ('maxTorque', c_float),
         ('maxPower', c_float),
-        ('maxRpm' , c_int32),
+        ('maxRpm', c_int32),
         ('maxFuel', c_float),
         ('suspensionMaxTravel', c_float * 4),
         ('tyreRadius', c_float * 4),
@@ -138,16 +164,30 @@ class SimInfo:
     def __del__(self):
         self.close()
 
+info = SimInfo()
+
 
 def demo():
     import time
-    
-    info = SimInfo()
+
     for _ in range(400):
-        print(info.static.track, info.graphics.tyreCompound, 
-              info.physics.rpms, info.graphics.currentTime)
+        print(info.static.track, info.graphics.tyreCompound, info.graphics.currentTime,
+              info.physics.rpms, info.graphics.currentTime, info.static.maxRpm, list(info.physics.tyreWear))
         time.sleep(0.1)
 
 
+def do_test():
+    for struct in info.static, info.graphics, info.physics:
+        print(struct.__class__.__name__)
+        for field, type_spec in struct._fields_:
+            if field.startswith("_"):
+                field = field[1:]
+            value = getattr(struct, field)
+            if not isinstance(value, (str, float, int)):
+                value = list(value)
+            print(" {} -> {}".format(field, value))
+
+
 if __name__ == '__main__':
+    do_test()
     demo()
